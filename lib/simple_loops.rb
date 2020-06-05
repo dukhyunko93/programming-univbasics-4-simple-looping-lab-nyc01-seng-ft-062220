5019ed67033d1a13f131bd7f28ab5bbf33c259a1
Duke Ko <dukhyunko93@gmail.com>
2020-06-05 13:27:30 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -7,3 +7,11 @@ def loop_message_five_times(message)
   puts message
   puts message
 end
+
+def loop_message_n_times(message, n)
+  until counter = n do
+    puts message
+    counter += 1
+  end
+end
+  
